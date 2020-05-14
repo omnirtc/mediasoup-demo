1b942a1a82c8e074046644bbf63e5197edadc0b7
omnirtc <57826089+omnirtc@users.noreply.github.com>
2020-05-14 15:10:30 +0800

    update packages and fix error

--- app/lib/urlFactory.js
+++ app/lib/urlFactory.js
@@ -2,6 +2,6 @@ export function getProtooUrl({ roomId, peerId })
 {
 	const host = window.location.host;
 	const path = window.location.pathname;
-	let url = `wss://${host}${path}?roomId=${roomId}&peerId=${peerId}`;
-	return `wss://${hostname}:${protooPort}/?roomId=${roomId}&peerId=${peerId}`;
+
+	return `wss://${host}${path}?roomId=${roomId}&peerId=${peerId}`;
 }
